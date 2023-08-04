665d797908156096d7dff7f8d9497b760d907110
theurs <theurs@gmail.com>
2023-08-04 21:59:03 +1000

    fix

--- my_bard.py
+++ my_bard.py
@@ -2,15 +2,13 @@
 
 
 import threading
-import re
+import random
 import requests
 
 from bardapi import Bard
-from textblob import TextBlob
 
 import cfg
 import my_log
-import utils
 
 
 # хранилище сессий {chat_id(int):session(bardapi.Bard),...}
@@ -21,15 +19,6 @@ CHAT_LOCKS = {}
 
 # максимальный размер запроса который принимает бард, получен подбором
 MAX_REQUEST = 3100
-
-
-# указатель на текущий ключ в списке ключей (токенов)
-current_token = 0
-# если задан всего 1 ключ то продублировать его, что бы было 2, пускай и одинаковые но 2
-if len(cfg.bard_tokens) == 1:
-    cfg.bard_tokens.append(cfg.bard_tokens[0])
-# на случай если все ключи протухли надо использовать счетчик что бы не попасть в петлю
-loop_detector = {}
 
 
 def get_new_session():
@@ -49,7 +38,8 @@ def get_new_session():
 
     session = requests.Session()
 
-    session.cookies.set("__Secure-1PSID", cfg.bard_tokens[current_token])
+    random.shuffle(cfg.bard_tokens)
+    session.cookies.set("__Secure-1PSID", cfg.bard_tokens[0])
 
     session.headers = {
         "Host": "bard.google.com",
@@ -60,15 +50,16 @@ def get_new_session():
         "Referer": "https://bard.google.com/",
         }
 
-    bard = Bard(token=cfg.bard_tokens[current_token], proxies=proxies, session=session, timeout=30)
+    for token in cfg.bard_tokens:
+        bard = Bard(token=token, proxies=proxies, session=session, timeout=30)
 
-    rules = """Отвечай на русском языке, коротко."""
+        rules = """Отвечай на русском языке, коротко."""
 
-    #my_log.log2(str(rules))
-    r = bard.get_answer(rules)
-    #my_log.log2(str(r))
+        r = bard.get_answer(rules)
+        if r['content']:
+            return bard
 
-    return bard
+    return None
 
 
 def reset_bard_chat(dialog: str):
@@ -109,6 +100,7 @@ def chat_request(query: str, dialog: str, reset = False) -> str:
         session = DIALOGS[dialog]
     else:
         session = get_new_session()
+        assert session != None, 'no bard session found'
         DIALOGS[dialog] = session
 
     try:
@@ -120,11 +112,12 @@ def chat_request(query: str, dialog: str, reset = False) -> str:
         try:
             del DIALOGS[dialog]
             session = get_new_session()
+            assert session != None, 'no bard session found'
             DIALOGS[dialog] = session
-        except KeyError:
-            print(f'no such key in DIALOGS: {dialog}')
-            my_log.log2(f'my_bard.py:chat_request:no such key in DIALOGS: {dialog}')
-
+        except Exception as error:
+            print(f'my_bard:chat_request: {error}')
+            my_log.log2(f'my_bard:chat_request: {error}')
+            return ''
         try:
             response = session.get_answer(query)
         except Exception as error2:
@@ -138,17 +131,6 @@ def chat_request(query: str, dialog: str, reset = False) -> str:
         links = list(set([x for x in response['links'] if 'http://' not in x]))
     except Exception as links_error:
         # вероятно получили ответ с ошибкой слишком частого доступа, надо сменить ключ
-        global current_token
-        if dialog in loop_detector:
-            loop_detector[dialog] += 1
-        else:
-            loop_detector[dialog] = 1
-        if loop_detector[dialog] >= len(cfg.bard_tokens):
-            loop_detector[dialog] = 0
-            return ''
-        current_token += 1
-        if current_token >= len(cfg.bard_tokens):
-            current_token = 0
         print(links_error)
         my_log.log2(f'my_bard.py:chat_request:bard token rotated:current_token: {current_token}\n\n{links_error}')
         chat_request(query, dialog, reset = True)
@@ -178,57 +160,10 @@ def chat_request(query: str, dialog: str, reset = False) -> str:
     #    print(error2)
     #    my_log.log2(str(error2))
 
-    if len(result) > 16000:
-        return result[:16000]
+    if len(result) > 4000:
+        return result[:4000]
     else:
         return result
-
-
-def chat_request_image(query: str, dialog: str, image: bytes, reset = False):
-    """
-    Function to make a chat request with an image.
-    
-    Args:
-        query (str): The query for the chat request.
-        dialog (str): The index of the dialog.
-        image (bytes): The image to be used in the chat request.
-        reset (bool, optional): Whether to reset the chat dialog. Defaults to False.
-    
-    Returns:
-        str: The response from the chat request.
-    """
-    if reset:
-        reset_bard_chat(dialog)
-        return
-
-    if dialog in DIALOGS:
-        session = DIALOGS[dialog]
-    else:
-        session = get_new_session()
-        DIALOGS[dialog] = session
-
-    try:
-        response = session.ask_about_image(query, image)['content']
-    except Exception as error:
-        print(error)
-        my_log.log2(str(error))
-
-        try:
-            del DIALOGS[dialog]
-            session = get_new_session()
-            DIALOGS[dialog] = session
-        except KeyError:
-            print(f'no such key in DIALOGS: {dialog}')
-            my_log.log2(f'my_bard.py:chat:no such key in DIALOGS: {dialog}')
-
-        try:
-            response = session.ask_about_image(query, image)['content']
-        except Exception as error2:
-            print(error2)
-            my_log.log2(str(error2))
-            return ''
-
-    return response
 
 
 def chat(query: str, dialog: str, reset: bool = False) -> str:
@@ -250,115 +185,6 @@ def chat(query: str, dialog: str, reset: bool = False) -> str:
         CHAT_LOCKS[dialog] = lock
     with lock:
         result = chat_request(query, dialog, reset)
-    return result
-
-
-def chat_image(query: str, dialog: str, image: bytes, reset: bool = False) -> str:
-    """
-    Executes a chat request with an image.
-
-    Args:
-        query (str): The query string for the chat request.
-        dialog (str): The ID of the dialog.
-        image (bytes): The image to be included in the chat request.
-        reset (bool, optional): Whether to reset the dialog state. Defaults to False.
-
-    Returns:
-        str: The response from the chat request.
-    """
-    if dialog in CHAT_LOCKS:
-        lock = CHAT_LOCKS[dialog]
-    else:
-        lock = threading.Lock()
-        CHAT_LOCKS[dialog] = lock
-    with lock:
-        result = chat_request_image(query, dialog, image, reset)
-    return result
-
-
-def split_text(text: str, max_size: int = MAX_REQUEST) -> list:
-    """
-    Split the given text into chunks of sentences, where each chunk does not exceed the maximum size.
-
-    Args:
-        text (str): The text to be split.
-        max_size (int, optional): The maximum size of each chunk. Defaults to MAX_REQUEST.
-
-    Returns:
-        list: A list of chunks, where each chunk contains a group of sentences.
-    """
-    if len(text) < 500:
-        return text
-
-    text = text.replace(u"\xa0\xa0", " ")
-    text = text.replace(u"\xa0", " ")
-
-    blob = TextBlob(text)
-    sentences = blob.sentences
-    chunk = ''
-    chunks = []
-    sentences2 = []
-
-    for sentence in sentences:
-        if len(sentence) > max_size-300:
-            sentences2 += [x for x in utils.split_text(sentence, int(max_size/2))]
-        else:
-            sentences2.append(sentence)
-
-    for sentence in sentences2:
-        sentence = sentence.replace("\n", " ")
-        sentence = re.sub(r'\s{2,}', ' ', str(sentence))
-        if len(chunk) + len(sentence) < max_size:
-            chunk += str(sentence) + ' '
-        else:
-            chunks.append(chunk)
-            chunk = sentence
-    if chunk:
-        chunks.append(chunk)
-    return chunks
-
-
-def bard_clear_text_chunk_voice(chunk: str) -> str:
-    """
-    Clears a text chunk from voice by making it more readable and correcting typical
-    voice recognition errors.
-
-    :param chunk: The text chunk to be cleared.
-    :type chunk: str
-    :return: The cleared text chunk.
-    :rtype: str
-    """
-    query = '''Исправь форматирование текста аудиосообщения, сделай его легко читаемым, разбей на абзацы,
-исправь характерные ошибки распознавания голоса, убери лишние переносы строк, в ответе должен быть
-только исправленный текст, максимально короткий ответ.
-
-
-''' + chunk
-
-    try:
-        response = chat(query, 0)
-    except Exception as error:
-        print(error)
-        my_log.log2(f'my_bard.py:bard_clear_text_chunk_voice:{error}')
-        return chunk
-
-    return response.strip()
-
-
-def clear_voice_message_text(text: str) -> str:
-    """
-    Clear the voice message text with using Bard AI.
-
-    Parameters:
-        text (str): The voice message text to be cleared.
-    
-    Returns:
-        str: The cleared voice message text as a single string.
-    """
-    result = ''
-    for chunk in split_text(text, 2500):
-        result += bard_clear_text_chunk_voice(chunk) + '\n\n'
-    result = result.strip()
     return result
 
 
