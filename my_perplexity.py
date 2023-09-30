9a3f39f751ff8d5c4281ec76298b2ac27d6b311f
theurs <theurs@gmail.com>
2023-09-30 10:26:33 +1000

    fix

--- my_perplexity.py
+++ my_perplexity.py
@@ -14,33 +14,32 @@ def ask(query: str, search_focus: str = 'internet') -> str:
 
     assert search_focus in ["internet", "scholar", "news", "youtube", "reddit", "wikipedia"], "Invalid search focus"
 
-    # пробуем 3 раза получить ответ
-    for _ in range(3):
-        try:
-            perplexity = Perplexity()
-            # answer = perplexity.search(query, search_focus=search_focus)
-            answer = ''
-            answer = perplexity.search(query)
-            result = ''
-            for i in answer:
-                d = json.loads(i['text'])
-                result += d['answer']
+    try:
+        perplexity = Perplexity()
+        answer = perplexity.search(query, search_focus=search_focus)
+        result = ''
+        for i in answer:
+            d = json.loads(i['text'])
+            result += d['answer']
+            try:
                 if i['step_type'] == 'FINAL':
                     result += '\n\n'
                     for x in d['web_results']:
                         result += f'<a href="{x["url"]}">{x["name"]}</a>\n'
-            answer = result
-        except Exception as error:
-            answer = ''
-            print(error)
-            my_log.log2(f'my_perplexity.py:ask: {error}')
+            except Exception as error:
+                if 'step_type' not in error:
+                    print(error)
+                    my_log.log2(f'my_perplexity.py:ask: {error}')
+    except Exception as error:
+        print(error)
+        my_log.log2(f'my_perplexity.py:ask: {error}')
  
-    return answer
+    return result
 
 
 if __name__ == '__main__':
 
-    print(ask('1+1'))
+    print(ask('курс рубля к доллару и прогноз'))
     while 1:
         query = input('> ')
         print(ask(query))
