0799ac879664fde9155ff3cf05255c35f408add2
theurs <theurs@gmail.com>
2023-10-04 17:40:47 +1000

    fix

--- tb.py
+++ tb.py
@@ -1408,14 +1408,14 @@ def do_task(message, custom_prompt: str = ''):
             msg = message.text.lower()
 
         # если есть запрещенный в opanai контент
-        try:
-            moderation_result = gpt_basic.moderation(message.text)
-        except Exception as error:
-            print(f'tb:do_task:moderation: {error}')
-            my_log.log2(f'tb:do_task:moderation: {error}')
-            moderation_result = ''
-        if moderation_result:
-            send_message_to_admin2(message, user_text, moderation_result)
+        # try:
+        #     moderation_result = gpt_basic.moderation(message.text)
+        # except Exception as error:
+        #     print(f'tb:do_task:moderation: {error}')
+        #     my_log.log2(f'tb:do_task:moderation: {error}')
+        #     moderation_result = ''
+        # if moderation_result:
+        #     send_message_to_admin2(message, user_text, moderation_result)
 
         # если есть совпадение в списке стоп слов
         # удаляем все символы кроме букв
