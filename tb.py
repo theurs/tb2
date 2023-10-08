4f7295979cb2d80d9378dc3408e44c704e447e12
theurs <theurs@gmail.com>
2023-10-09 00:57:40 +1000

    fix

--- tb.py
+++ tb.py
@@ -1658,7 +1658,7 @@ def main():
     Runs the main function, which sets default commands and starts polling the bot.
     """
     #set_default_commands()
-    bot.polling(timeout=90, long_polling_timeout = 90, skip_pending=True, logger_level=logging.INFO)
+    bot.polling(timeout=90, long_polling_timeout = 90)
 
 
 if __name__ == '__main__':
