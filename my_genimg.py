1f813311165511adfa32e70008fa00b50cae0337
theurs <theurs@gmail.com>
2023-10-20 23:29:31 +1000

    fix

--- my_genimg.py
+++ my_genimg.py
@@ -10,7 +10,7 @@ import my_log
 def openai(prompt: str):
     """рисует 4 картинок с помощью openai и возвращает сколько смог нарисовать"""
     try:
-        return gpt_basic.image_gen(prompt, amount = 4)
+        return gpt_basic.image_gen(prompt, amount = 6)
     except Exception as error_openai_img:
         print(f'my_genimg:openai: {error_openai_img}')
         my_log.log2(f'my_genimg:openai: {error_openai_img}')
