5c9c9ab3b304d14e47262c510ac40ece9c135290
theurs <theurs@gmail.com>
2024-01-20 11:38:55 +1000

    fix

--- my_log.py
+++ my_log.py
@@ -41,7 +41,6 @@ def log_huggin_face_api(text: str) -> None:
         time_now = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
         log_file_path = 'logs/debug_hugging_face_api.log'
         open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
-        open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
 
 
 def log_translate(text: str) -> None:
@@ -51,7 +50,6 @@ def log_translate(text: str) -> None:
         time_now = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
         log_file_path = 'logs/debug_translate.log'
         open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
-        open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
 
 
 def log_echo(message: telebot.types.Message, reply_from_bot: str = '', debug: bool = False) -> None:
