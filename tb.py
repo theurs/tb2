a3338b43221777f8dea1294e7a6e20f65d16b6ed
theurs <theurs@gmail.com>
2024-02-05 11:54:22 +1000

    fix

--- tb.py
+++ tb.py
@@ -1565,11 +1565,11 @@ def reply_to_long_message(message: telebot.types.Message, resp: str, parse_mode:
 
     preview = telebot.types.LinkPreviewOptions(is_disabled=disable_web_page_preview)
 
-    if len(resp) < 20000:
+    if len(resp) < 32000:
         if parse_mode == 'HTML':
-            chunks = utils.split_html(resp, 4000)
+            chunks = utils.split_html(resp, 3800)
         else:
-            chunks = utils.split_text(resp, 4000)
+            chunks = utils.split_text(resp, 3800)
         for chunk in chunks:
             try:
                 bot.reply_to(message, chunk, parse_mode=parse_mode,
