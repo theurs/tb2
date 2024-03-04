fcd3f23d2ae9ee6bd7a0648ddf3c290e221892bb
theurs <theurs@gmail.com>
2024-03-04 18:04:10 +1000

    fix

--- my_genimg.py
+++ my_genimg.py
@@ -3,34 +3,48 @@
 
 import base64
 import json
+import os
 import random
+import sys
 import time
 import traceback
 from multiprocessing.pool import ThreadPool
 
+import gradio_client
 import langdetect
 import requests
+from duckduckgo_search import DDGS
 from sqlitedict import SqliteDict
 
-import cfg
 import bing_img
+import cfg
+import gpt_basic
 import my_gemini
 import my_log
+import my_trans
 
 
-DEBUG = False
+DEBUG = cfg.DEBUG if hasattr(cfg, 'DEBUG') else False
+
+
+NFSW_CONTENT = SqliteDict('db/nfsw_content_stable_diffusion.db', autocommit=True)
 
 
 # {hash of image:model name, ...}
 WHO_AUTOR = {}
 
 
-def bing(prompt: str, moderation_flag: bool = False):
+# запоминаем промпты для хаггинг фейса, они не должны повторятся
+# {prompt:True/False, ...}
+huggingface_prompts = SqliteDict('db/kandinski_prompts.db', autocommit=True)
+
+
+def bing(prompt: str, moderation_flag: bool = False, user_id: str = ''):
     """рисует 4 картинки с помощью далли и возвращает сколько смог нарисовать"""
     if moderation_flag:
         return []
     try:
-        images = bing_img.gen_images(prompt)
+        images = bing_img.gen_images(prompt, user_id)
         if type(images) == list:
             return images
     except Exception as error_bing_img:
@@ -38,21 +52,64 @@ def bing(prompt: str, moderation_flag: bool = False):
     return []
 
 
+def openai(prompt: str):
+    """рисует 4 картинки с помощью openai и возвращает сколько смог нарисовать"""
+    try:
+        return gpt_basic.image_gen(prompt, amount = 4)
+    except Exception as error_openai_img:
+        print(f'my_genimg:openai: {error_openai_img}')
+        my_log.log2(f'my_genimg:openai: {error_openai_img}')
+    return []
 
-def rewrite_prompt_for_open_dalle(prompt: str) -> str:
-    """
-    Generate a new prompt for OpenDalle image generation by rewriting the given prompt.
+
+def ddg_search_images(prompt: str, max_results: int = 10):
+    """ищет картинки в поисковике"""
+    result = []
+    try:
+        images = DDGS().images(prompt, size='Large', safesearch='on', license_image='Share')
+        for image in images:
+            result.append(image['image'])
+            if len(result) > 20:
+                break
+    except Exception as error_ddg_img:
+        print(f'my_genimg:ddg: {error_ddg_img}')
+        my_log.log2(f'my_genimg:ddg: {error_ddg_img}')
+    random.shuffle(result)
+    return result[:max_results]
+
+
+def wizmodel_com(prompt: str):
+    if not hasattr(cfg, 'WIZMODEL_API') or not cfg.WIZMODEL_API:
+        return []
+
+    url = "https://api.wizmodel.com/v1/predictions"
     
-    Args:
-        prompt (str): The original prompt for image generation.
-        
-    Returns:
-        str: The rewritten prompt in English.
-    """
-    prompt_translated = my_gemini.ai(f'This is a prompt for image generation. Rewrite it in english, in one long sentance, make it better:\n\n{prompt}', temperature=1)
-    if not prompt_translated:
-        return translate_prompt_to_en(prompt)
-    return translate_prompt_to_en(prompt_translated)
+    if cfg.bing_proxy:
+        proxy = {'http': random.choice(cfg.bing_proxy), 'https': random.choice(cfg.bing_proxy)}
+    else:
+        proxy = None
+
+    payload = json.dumps({
+        "input": {
+            "prompt": prompt
+            },
+        "version": "7d229e3ed5d01c879622d0cd273572260b7e35103d6765af740f853b160d04b7"
+        }
+                         )
+
+    api_key = random.choice(cfg.WIZMODEL_API[0])
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}'
+        }
+
+    try:
+        response = requests.request("POST", url, headers=headers, data=payload, timeout = 200, proxies=proxy)
+    except Exception as error:
+        my_log.log2(f'my_genimg:wizmodel_com: {error}\n\nPrompt: {prompt}')
+        return []
+
+    return response.text
 
 
 def translate_prompt_to_en(prompt: str) -> str:
@@ -69,10 +126,127 @@ def translate_prompt_to_en(prompt: str) -> str:
     if detected_lang != 'en':
         prompt_translated = my_gemini.translate(prompt, to_lang='en', help='This is a prompt for image generation. Users can write it in their own language, but only English is supported.')
         if not prompt_translated:
-            prompt_translated = prompt
+            prompt_translated = my_trans.translate_text2(prompt, 'en')
         if prompt_translated:
             prompt = prompt_translated
     return prompt
+
+
+def rewrite_prompt_for_open_dalle(prompt: str) -> str:
+    """
+    Generate a new prompt for OpenDalle image generation by rewriting the given prompt.
+    
+    Args:
+        prompt (str): The original prompt for image generation.
+        
+    Returns:
+        str: The rewritten prompt in English.
+    """
+    # small text detect fails :(
+
+    force = False
+    if hash(prompt) in huggingface_prompts:
+        force = True
+    else:
+        huggingface_prompts[hash(prompt)] = True
+
+    detected_lang = langdetect.detect(prompt)
+    if detected_lang != 'en' or force:
+        prompt_translated = my_gemini.ai(f'This is a prompt for image generation. Rewrite it in english, in one long sentance, make it better:\n\n{prompt}', temperature=1)
+        if not prompt_translated:
+            return translate_prompt_to_en(prompt)
+        return translate_prompt_to_en(prompt_translated)
+    else:
+        return prompt
+
+
+def stable_duffision_api(prompt: str):
+    """
+    Requests an image from the Stable Diffusion API using the provided prompt.
+
+    Args:
+        prompt (str): The prompt for generating the image.
+
+    Returns:
+        list[str]: A list containing the URL of the generated image if successful, otherwise an empty list.
+    """
+    try:
+        if hasattr(cfg, 'STABLE_DIFFUSION_API') and cfg.STABLE_DIFFUSION_API:
+            if prompt in NFSW_CONTENT:
+                return []
+            url = "https://stablediffusionapi.com/api/v3/text2img"
+
+            prompt = translate_prompt_to_en(prompt)
+
+            api_keys = cfg.STABLE_DIFFUSION_API[:]
+            random.shuffle(api_keys)
+            for api_key in api_keys:
+                payload = json.dumps({
+                    "key": api_key,
+                    "prompt": prompt,
+                    "negative_prompt": None,
+                    "width": "1024",
+                    "height": "1024",
+                    "samples": "1",
+                    "num_inference_steps": "20",
+                    "seed": None,
+                    "guidance_scale": 7.5,
+                    "safety_checker": "yes",
+                    "multi_lingual": "no",
+                    "panorama": "no",
+                    "self_attention": "no",
+                    "upscale": "no",
+                    "embeddings_model": None,
+                    "webhook": None,
+                    "track_id": None
+                })
+                headers = {
+                'Content-Type': 'application/json'
+                }
+                response = requests.request("POST", url, headers=headers, data=payload, timeout=60)
+                response_json = json.loads(response.text)
+
+                # Get the fields from the JSON response
+                status = response_json["status"]
+                if status == "success":
+                    # generation_time = response_json["generationTime"]
+                    # request_id = response_json["id"]
+                    image_url = response_json["output"][0]
+                    # proxy_link = response_json["proxy_links"][0]
+                    nsfw_content_detected = response_json["nsfw_content_detected"]
+                    if nsfw_content_detected:
+                        NFSW_CONTENT[prompt] = nsfw_content_detected
+                        return []
+                    # meta = response_json["meta"]
+
+                    # Extract the meta fields
+                    # image_height = meta["H"]
+                    # image_width = meta["W"]
+                    # enable_attention_slicing = meta["enable_attention_slicing"]
+                    # file_prefix = meta["file_prefix"]
+                    # guidance_scale = meta["guidance_scale"]
+                    # instant_response = meta["instant_response"]
+                    # model_name = meta["model"]
+                    # num_samples = meta["n_samples"]
+                    # negative_prompt = meta["negative_prompt"]
+                    # output_directory = meta["outdir"]
+                    # prompt = meta["prompt"]
+                    # model_revision = meta["revision"]
+                    # safety_checker = meta["safetychecker"]
+                    # seed = meta["seed"]
+                    # steps = meta["steps"]
+                    # temperature = meta["temp"]
+                    # vae_model = meta["vae"]
+                    return [image_url,]
+                else:
+                    try:
+                        my_log.log_stable_diffusion_api(f'{response_json}\n\nStatus: {status}\nMessage: {response_json["message"]}\nPrompt: {prompt}\nAPI key: {api_key}')
+                    except:
+                        my_log.log_stable_diffusion_api(f'{response_json}\n\nPrompt: {prompt}\nAPI key: {api_key}')
+    except Exception as unknown:
+        error_traceback = traceback.format_exc()
+        my_log.log2(f'my_genimg:stable_diffusion_api: {str(unknown)}\n\n{error_traceback}')
+    return []
 
 
 def huggin_face_api(prompt: str) -> bytes:
@@ -88,23 +262,32 @@ def huggin_face_api(prompt: str) -> bytes:
     if not hasattr(cfg, 'huggin_face_api'):
         return []
 
-    API_URL = [
-        "https://api-inference.huggingface.co/models/stablediffusionapi/juggernaut-xl-v8",
-        "https://api-inference.huggingface.co/models/stablediffusionapi/juggernaut-xl-v8",
+    if hasattr(cfg, 'huggin_face_models_urls') and cfg.huggin_face_models_urls:
+        API_URL = cfg.huggin_face_models_urls
+    else:
+        API_URL = [
+            "https://api-inference.huggingface.co/models/stablediffusionapi/juggernaut-xl-v8",
+            'playgroundai/playground-v2.5-1024px-aesthetic',
+            "https://api-inference.huggingface.co/models/ehristoforu/dalle-3-xl",
+            "https://api-inference.huggingface.co/models/ehristoforu/dalle-3-xl",
+            'playgroundai/playground-v2.5-1024px-aesthetic',
+            "multimodalart/stable-cascade",
+        ]
 
-        "https://api-inference.huggingface.co/models/ehristoforu/dalle-3-xl",
-        "https://api-inference.huggingface.co/models/ehristoforu/dalle-3-xl",
-        "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
-
-        "https://api-inference.huggingface.co/models/cagliostrolab/animagine-xl-3.0",
-    ]
-
-    # prompt = translate_prompt_to_en(prompt)
+    prompt_ = prompt
     prompt = rewrite_prompt_for_open_dalle(prompt)
+    if prompt_ != prompt:
+        my_log.log_reprompts(f'{prompt_}\n\n{prompt}')
+
     payload = json.dumps({"inputs": prompt})
 
     def request_img(prompt, url, p):
-        n = 10
+        if 'stable-cascade' in url:
+            return stable_cascade(prompt, url)
+        if 'playgroundai/playground-v2.5-1024px-aesthetic' in url:
+            return playground25(prompt, url)
+
+        n = 1
         result = []
         while n > 0:
             n -= 1
@@ -123,8 +306,9 @@ def huggin_face_api(prompt: str) -> bytes:
                 continue
 
             resp_text = str(response.content)[:300]
-            # print(resp_text[:60])
-            if 'read timeout=' in resp_text: # и так долго ждали
+            # if isinstance(resp_text, str):
+            #     print(resp_text[:300])
+            if 'read timeout=' in resp_text or "SOCKSHTTPSConnectionPool(host='api-inference.huggingface.co', port=443): Max retries exceeded with url" in resp_text: # и так долго ждали
                 return []
             if response.content and '{"error"' not in resp_text:
                 result.append(response.content)
@@ -143,18 +327,106 @@ def huggin_face_api(prompt: str) -> bytes:
 
         return result
 
-    pool = ThreadPool(processes=6)
-    async_result1 = pool.apply_async(request_img, (prompt, API_URL[0], payload,))
-    async_result2 = pool.apply_async(request_img, (prompt, API_URL[1], payload,))
-    async_result3 = pool.apply_async(request_img, (prompt, API_URL[2], payload,))
-    async_result4 = pool.apply_async(request_img, (prompt, API_URL[3], payload,))
-    async_result5 = pool.apply_async(request_img, (prompt, API_URL[4], payload,))
-    async_result6 = pool.apply_async(request_img, (prompt, API_URL[5], payload,))
-    result = async_result1.get() + async_result2.get() + async_result3.get() + async_result4.get() + async_result5.get() + async_result6.get()
+    pool = ThreadPool(processes=len(API_URL))
+    async_results = []
+    for x in API_URL:
+        async_results.append(pool.apply_async(request_img, (prompt, x, payload,)))
+
+    result = []
+    for x in async_results:
+        result += x.get()
 
     result = list(set(result))
 
     return result
+
+
+def playground25(prompt: str, url: str) -> bytes:
+    """
+    url = "playgroundai/playground-v2.5-1024px-aesthetic" only?
+    """
+    client = gradio_client.Client("https://playgroundai-playground-v2-5.hf.space/--replicas/9kuov/")
+    result = None
+    try:
+        result = client.predict(
+            prompt,	# str  in 'Prompt' Textbox component
+            "",	# str  in 'Negative prompt' Textbox component
+            False,	# bool  in 'Use negative prompt' Checkbox component
+            random.randint(0, 2147483647),	    # float (numeric value between 0 and 2147483647) in 'Seed' Slider component
+            1024,	# float (numeric value between 256 and 1536) in 'Width' Slider component
+            1024,	# float (numeric value between 256 and 1536) in 'Height' Slider component
+            3,	# float (numeric value between 0.1 and 20) in 'Guidance Scale' Slider component
+            True,	# bool  in 'Randomize seed' Checkbox component
+            api_name="/run"
+        )
+    except Exception as error:
+        if 'No GPU is currently available for you after 60s' not in str(error) and 'You have exceeded your GPU quota' not in str(error):
+            my_log.log2(f'my_genimg:stable_cascade: {error}\n\nPrompt: {prompt}\nURL: {url}')
+        return []
+
+    fname = result[0][0]['image']
+    base_path = os.path.dirname(fname)
+    if fname:
+        try:
+            data = None
+            with open(fname, 'rb') as f:
+                data = f.read()
+            try:
+                os.remove(fname)
+                os.rmdir(base_path)
+            except Exception as error:
+                my_log.log2(f'my_genimg:stable_cascade: {error}\n\nPrompt: {prompt}\nURL: {url}')
+            if data:
+                WHO_AUTOR[hash(data)] = url.split('/')[-1]
+                return [data,]
+        except Exception as error:
+            my_log.log2(f'my_genimg:stable_cascade: {error}\n\nPrompt: {prompt}\nURL: {url}')
+    return []
+
+
+def stable_cascade(prompt: str, url: str) -> bytes:
+    """
+    url = "multimodalart/stable-cascade" only?
+    """
+    client = gradio_client.Client(url)
+    result = None
+    try:
+        result = client.predict(
+            prompt,	# str  in 'Prompt' Textbox component
+            "",	# str  in 'Negative prompt' Textbox component
+            0,	# float (numeric value between 0 and 2147483647) in 'Seed' Slider component
+            1024,	# float (numeric value between 1024 and 1536) in 'Width' Slider component
+            1024,	# float (numeric value between 1024 and 1536) in 'Height' Slider component
+            10,	# float (numeric value between 10 and 30) in 'Prior Inference Steps' Slider component
+            0,	# float (numeric value between 0 and 20) in 'Prior Guidance Scale' Slider component
+            4,	# float (numeric value between 4 and 12) in 'Decoder Inference Steps' Slider component
+            0,	# float (numeric value between 0 and 0) in 'Decoder Guidance Scale' Slider component
+            1,	# float (numeric value between 1 and 2) in 'Number of Images' Slider component
+            api_name="/run"
+        )
+    except Exception as error:
+        if 'No GPU is currently available for you after 60s' not in str(error) and 'You have exceeded your GPU quota' not in str(error):
+            my_log.log2(f'my_genimg:stable_cascade: {error}\n\nPrompt: {prompt}\nURL: {url}')
+        return []
+
+    fname = result
+    base_path = os.path.dirname(fname)
+    if fname:
+        try:
+            data = None
+            with open(fname, 'rb') as f:
+                data = f.read()
+            try:
+                os.remove(fname)
+                os.rmdir(base_path)
+            except Exception as error:
+                my_log.log2(f'my_genimg:stable_cascade: {error}\n\nPrompt: {prompt}\nURL: {url}')
+            if data:
+                WHO_AUTOR[hash(data)] = url.split('/')[-1]
+                return [data,]
+        except Exception as error:
+            my_log.log2(f'my_genimg:stable_cascade: {error}\n\nPrompt: {prompt}\nURL: {url}')
+    return []
 
 
 def kandinski(prompt: str, width: int = 1024, height: int = 1024, num: int = 1):
@@ -228,7 +500,7 @@ def kandinski(prompt: str, width: int = 1024, height: int = 1024, num: int = 1):
         my_log.log2(f'my_genimg:kandinski: {error}\n\n{error_traceback}')
 
 
-def gen_images(prompt: str, moderation_flag: bool = False):
+def gen_images(prompt: str, moderation_flag: bool = False, user_id: str = ''):
     """рисует одновременно всеми доступными способами"""
     #return bing(prompt) + chimera(prompt)
 
@@ -236,25 +508,35 @@ def gen_images(prompt: str, moderation_flag: bool = False):
 
     pool = ThreadPool(processes=6)
 
-    async_result1 = pool.apply_async(bing, (prompt, moderation_flag))
+    async_result1 = pool.apply_async(bing, (prompt, moderation_flag, user_id))
 
     async_result2 = pool.apply_async(huggin_face_api, (prompt,))
-
+    
     async_result3 = pool.apply_async(kandinski, (prompt,))
     
     async_result4 = pool.apply_async(kandinski, (prompt,))
 
     result = async_result1.get() + async_result2.get() + async_result3.get() + async_result4.get()
+
     return result[:10]
 
 
 if __name__ == '__main__':
-    n=0
-    t = my_gemini.ai('Напиши промпт для рисования красивой картинки, сделай одно предложение.', temperature=1)
-    starttime=time.time()
-    print(t)
-    for x in huggin_face_api(t):
-        n+=1
-        open(f'{n}.jpg','wb').write(x)
-    endtime=time.time()
-    print(round(endtime - starttime, 2))
+
+    print(huggin_face_api('An austronaut is sitting on a moon.'))
+
+    # if len(sys.argv) > 1:
+    #     t = ' '.join(sys.argv[1:])
+    # else:
+    #     t = my_gemini.ai('Write a prompt for drawing a beautiful picture, make one sentence.', temperature=1)
+
+    # n=0
+
+    # r = str(random.randint(1000000000,9000000000))
+    # starttime=time.time()
+    # print(t)
+    # for x in huggin_face_api(t):
+    #     n+=1
+    #     open(f'{n} - {r}.jpg','wb').write(x)
+    # endtime=time.time()
+    # print(round(endtime - starttime, 2))
