5bb07c8f957a76e57270133d5d872a219ea660eb
theurs <theurs@gmail.com>
2024-04-09 10:00:28 +1000

    fix

--- my_genimg.py
+++ my_genimg.py
@@ -571,10 +571,7 @@ def gen_images(prompt: str, moderation_flag: bool = False, user_id: str = ''):
 
     # async_result4 = pool.apply_async(kandinski, (prompt,))
 
-    async_result5 = pool.apply_async(stability_ai, (prompt,))
-    async_result6 = pool.apply_async(stability_ai, (prompt,))
-
-    result = (async_result1.get() or []) + (async_result5.get() or []) + (async_result6.get() or []) + (async_result3.get() or []) + (async_result2.get() or [])
+    result = (async_result1.get() or []) + (async_result3.get() or []) + (async_result2.get() or [])
 
     return result[:10]
 
