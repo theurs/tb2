fdbdc719fe5f9f2587aa3402a71c03971b68fd6b
theurs <theurs@gmail.com>
2024-04-24 08:02:06 +1000

    fix

--- my_genimg.py
+++ my_genimg.py
@@ -147,9 +147,9 @@ def huggin_face_api(prompt: str) -> bytes:
         API_URL = cfg.huggin_face_models_urls
     else:
         API_URL = [
+            'playgroundai/playground-v2.5-1024px-aesthetic',
             "https://api-inference.huggingface.co/models/ehristoforu/dalle-3-xl-v2",
             'AP123/SDXL-Lightning',
-            'playgroundai/playground-v2.5-1024px-aesthetic',
             "multimodalart/stable-cascade",
             "https://api-inference.huggingface.co/models/digiplay/Juggernaut_final",
             "https://api-inference.huggingface.co/models/dataautogpt3/TempestV0.1",
